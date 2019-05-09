48ab63937aadda4f3cb95fe6301663ec681e35b9
Ikenna N. Okpala <me@ikennaokpala.com>
2019-05-09 14:02:20 +0100

    Basic home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def index
+    render json: { see: 'https://small-project-api.herokuapp.com/api-docs' }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application#index'
+
   concern :v1_api_routes_concern do
     resources :ideas, only: %i[create destroy update index], module: :v1
     resources :users, only: %i[create], module: :v1
